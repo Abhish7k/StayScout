f2d364717354f8cb1ae002eddcfc19f68f071b14
Abhishek <abhishekbhosale.dev@gmail.com>
2024-06-12 19:22:54 +0530

    feat: categories filter
    created categories filter slider on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import MapFilterItems from "@/components/MapFilterItems";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
+    </main>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import UserNav from "./UserNav";
 export default function Navbar() {
   return (
     <div className="w-full border-b">
-      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-4">
         <Link href="/" className="text-3xl font-semibold text-primary">
           <span>stayscout</span>
         </Link>
