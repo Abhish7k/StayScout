cfcac30e54b4d741e0a951f7f5e14c11fe7fc813
Abhishek <abhishekbhosale.dev@gmail.com>
2024-06-12 17:09:34 +0530

    feat: navbar
    created the navbar with layout and styles

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import UserNav from "./UserNav";
+
+export default function Navbar() {
+  return (
+    <div className="w-full border-b">
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+        <Link href="/" className="text-3xl font-semibold text-primary">
+          <span>stayscout</span>
+        </Link>
+
+        <div className="rounded-full border px-5 py-2">
+          <h1>Hello from search</h1>
+        </div>
+
+        <UserNav />
+      </div>
+    </div>
+  );
+}
