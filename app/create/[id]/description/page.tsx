08b06cf1833e735f06b9bdb07f708c69f214c1dc
Abhishek <abhishekbhosale.dev@gmail.com>
2024-06-15 15:37:10 +0530

    feat: decription page form
    created the entire describe route for create you home route

--- app/create/[id]/description/page.tsx
+++ app/create/[id]/description/page.tsx
@@ -1,7 +1,90 @@
+"use client";
+
+import { Counter } from "@/components/Counter";
+import CreationBottomBar from "@/components/CreationBottomBar";
+import { Card, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@radix-ui/react-label";
+
 export default function DescriptionPage() {
   return (
-    <div>
-      <h1>hello from the DescriptionPage</h1>
-    </div>
+    <>
+      <div className="w-3/5 mx-auto">
+        <h1 className="text-3xl font-semibold tracking-tight transition-colors">
+          Please describe your home as good as you can!
+        </h1>
+      </div>
+
+      <form>
+        <div className="mx-auto w-3/5 mt-10 flex flex-col gap-y-5 mb-36">
+          <div className="flex flex-col gap-y-2">
+            <Label>Title</Label>
+            <Input name="title" required placeholder="short and simple" />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Descrption</Label>
+            <Textarea
+              name="description"
+              required
+              placeholder="Please describe your home..."
+            />
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <Label>Price</Label>
+            <Input
+              name="price"
+              type="number"
+              required
+              placeholder="Price per Night in USD"
+              min={10}
+            />
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <Label>Image</Label>
+            <Input name="image" type="file" required />
+          </div>
+
+          <Card>
+            <CardHeader className="flex flex-col gap-y-5">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">Guests</h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many guests do you want?
+                  </p>
+                </div>
+
+                <Counter name="guest" />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">Rooms</h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many rooms do you have?
+                  </p>
+                </div>
+
+                <Counter name="room" />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">Bathrooms</h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many bathrooms do you have?
+                  </p>
+                </div>
+
+                <Counter name="bathroom" />
+              </div>
+            </CardHeader>
+          </Card>
+        </div>
+
+        <CreationBottomBar />
+      </form>
+    </>
   );
 }
