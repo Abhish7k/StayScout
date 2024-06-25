85fdcc69939d704df8a34091dd6797546a07798e
Abhishek <abhishekbhosale.dev@gmail.com>
2024-06-25 14:30:21 +0530

    feat: address page
    created the address page

---
+++ app/create/[id]/address/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { createLocation } from "@/app/actions";
+import CreationBottomBar from "@/components/CreationBottomBar";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useCountries } from "@/lib/countries";
+import { useState } from "react";
+
+export default function AddressPage({ params }: { params: { id: string } }) {
+  const { getAllCountries } = useCountries();
+  const [locationValue, setLocationValue] = useState("");
+
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors mb-10">
+          Where is your Home located?
+        </h2>
+      </div>
+
+      <form action={createLocation}>
+        <input type="hidden" name="homeId" value={params.id} />
+        <input type="hidden" name="countryValue" value={locationValue} />
+        <div className="w-3/5 mx-auto mb-36">
+          <div className="mb-5">
+            <Select required onValueChange={(value) => setLocationValue(value)}>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select a Country" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Countries</SelectLabel>
+                  {getAllCountries().map((item) => (
+                    <SelectItem key={item.value} value={item.value}>
+                      {item.flag} {item.label} / {item.region}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+
+        <CreationBottomBar />
+      </form>
+    </>
+  );
+}
